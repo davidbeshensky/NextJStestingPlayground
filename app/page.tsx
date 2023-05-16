3af28feadcbad1161cbb266ece133deb9f50253a
David Beshensky <beshenskydave@gmail.com>
2023-05-16 15:57:58 -0500

    last commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,17 @@
 import ParentComponent from "./components/ParentComponent"
+import { SignedIn, SignedOut, SignIn, UserButton } from "@clerk/nextjs";
 
 export default function Home() {
   return (
     
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <ParentComponent />
+      <SignedOut>
+        <SignIn />
+      </SignedOut>
+      <SignedIn>
+        <UserButton />
+        <ParentComponent />
+      </SignedIn>
     </main>
   )
 }
